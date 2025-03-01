82fd673adb49c6baf789d794fee6546302b7c44c
shelter <arturshelter@gmail.com>
2025-03-01 14:51:09 +0200

    add footer

--- dyvo-ranok-app/src/app/app.component.ts
+++ dyvo-ranok-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TestDirComponent } from './test-dir/test-dir.component';
 import { MmHeadComponent } from "./mm-head/mm-head.component";
+import { MmFooterComponent } from "./mm-footer/mm-footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [TestDirComponent, MmHeadComponent],
+  imports: [TestDirComponent, MmHeadComponent, MmFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
